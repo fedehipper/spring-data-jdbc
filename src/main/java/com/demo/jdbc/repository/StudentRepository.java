403029824e8796891dffbe357917a676646e27de
fedehipper <federicohipper@gmail.com>
2020-07-11 21:59:04 -0300

    fefactor

--- src/main/java/com/demo/jdbc/repository/StudentRepository.java
+++ src/main/java/com/demo/jdbc/repository/StudentRepository.java
@@ -2,12 +2,10 @@ package com.demo.jdbc.repository;
 
 import com.demo.jdbc.domain.Student;
 import java.util.List;
-import org.springframework.data.jdbc.repository.query.Modifying;
 import org.springframework.data.repository.CrudRepository;
 
 public interface StudentRepository extends CrudRepository<Student, Long> {
     
-    @Modifying
     List<Student> findByName(String name);
     
 }
