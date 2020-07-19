d7dce19653622eef288869e4f5322d444d35f6ce
fedehipper <federicohipper@gmail.com>
2020-07-19 17:45:46 -0300

    creo tablas alumno y curso

--- src/test/java/com/demo/jdbc/business/service/AlumnoServiceTest.java
+++ src/test/java/com/demo/jdbc/business/service/AlumnoServiceTest.java
@@ -4,7 +4,10 @@ import com.demo.jdbc.JdbcApplicationTests;
 import com.demo.jdbc.domain.Alumno;
 import com.demo.jdbc.repository.AlumnoRepository;
 import java.time.LocalDate;
+import java.util.NoSuchElementException;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -16,10 +19,11 @@ public class AlumnoServiceTest extends JdbcApplicationTests {
     @Autowired
     private AlumnoRepository alumnoRepository;
 
-//    @BeforeEach
-//    public void setUp() {
-//        alumnoRepository.deleteAll();
-//    }
+    @BeforeEach
+    public void setUp() {
+        alumnoRepository.deleteAll();
+    }
+    
     @Test
     public void darDeAlta_alumnoNuevo_retornaAlumnoCreado() {
         Alumno alumno = new Alumno();
@@ -54,12 +58,20 @@ public class AlumnoServiceTest extends JdbcApplicationTests {
         alumno.setApellidoYNombre("Hipperdinger Federico");
         alumno.setFechaNacimiento(LocalDate.of(1992, 2, 28));
         alumno.setRegional("Buenos Aires");
-        
+
         Long alumnoId = alumnoRepository.save(alumno).getId();
-        
+
         Alumno alumnoEncontrado = alumnoService.buscarPorId(alumnoId);
-        
+
         assertThat(alumnoEncontrado.getId()).isEqualTo(alumnoId);
+    }
+
+    @Test
+    public void buscarPorId_alumnoNoExiste_lanzaException() {
+        Long alumnoIdNoExiste = alumnoRepository.count() + 1;
+
+        assertThatThrownBy(() -> alumnoService.buscarPorId(alumnoIdNoExiste))
+                .isInstanceOf(NoSuchElementException.class);
     }
 
 }
